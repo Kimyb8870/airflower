4e02b95dc73260b3e85041df703949c5039ab8c0
kimyb <qhd0084@gmail.com>
2024-06-12 01:04:17 +0900

    Temp: interval

--- src/routes/system/system.ctrl.js
+++ src/routes/system/system.ctrl.js
@@ -4,6 +4,36 @@ const { ApiResponse } = require("../../model/general");
 const CurrentSystemUpdateParam = require("../../model/system/CurrentSystemUpdateParam");
 const SystemModeUpdateParam = require("../../model/system/SystemModeUpdateParam");
 
+const MODE_INFO = {
+  intervalId: null,
+};
+
+const startAIInterval = () => {
+  MODE_INFO.intervalId = setInterval(() => {
+    console.log("interval activating");
+  }, 1000);
+};
+
+const clearAIInterval = () => {
+  clearInterval(MODE_INFO.intervalId);
+};
+
+// load current mode only for initialize MODE_INFO
+(async () => {
+  const modeInfo = await systemDB.selectCurrentSystem();
+  const { CURRENT_MODE } = modeInfo.getData();
+
+  switch (CURRENT_MODE) {
+    case 1: {
+      startAIInterval();
+      break;
+    }
+    case (0, 2, 3): {
+      break;
+    }
+  }
+})();
+
 const handleUpdateMode = async (req, res) => {
   const { MODE } = req.body;
 
@@ -12,6 +42,30 @@ const handleUpdateMode = async (req, res) => {
   const systemModeUpdateParam = new SystemModeUpdateParam(MODE);
 
   const sqlResult = await systemDB.updateSystemMode(systemModeUpdateParam);
+
+  switch (MODE) {
+    case 0: {
+      clearAIInterval();
+      console.log("Sleep Mode");
+      break;
+    }
+    case 1: {
+      const TEN_MIN = 1000;
+      console.log("A.I Mode");
+      startAIInterval();
+      break;
+    }
+    case 2: {
+      clearAIInterval();
+      console.log("Preset Mode");
+      break;
+    }
+    case 3: {
+      clearAIInterval();
+      console.log("Manual Mode");
+      break;
+    }
+  }
 
   if (sqlResult.isSuccess()) {
     apiResponse = new ApiResponse(true);
