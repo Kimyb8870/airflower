aa106c8b65eb008f8fb623af487b5826213a348f
kimyb <qhd0084@gmail.com>
2024-06-11 11:06:45 +0900

    Chore: make-table.sql, insert-default-data.sql

---
+++ src/asset/insert-default-data.sql
@@ -0,0 +1,10 @@
+INSERT INTO airflower.TB_SYSTEM_CODE (MODE,MODE_NAME) VALUES
+	 (0,'Sleep'),
+	 (1,'A.I'),
+	 (2,'Preset'),
+	 (3,'Manual');
+
+
+INSERT INTO airflower.TB_ACTION_CODE (ACTION_TYPE,ACTION_NAME,ACTION_IR_CODE) VALUES
+	 (0,'PowerOff','0'),
+	 (1,'PowerOn','1');
